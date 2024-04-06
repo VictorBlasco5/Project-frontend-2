7121e0d73a8dc525b7a355b2a7fbe589323fbf05
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-06 13:37:49 +0200

    feat: GetProfile function in apiCalls and logic implemented in profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,7 +2,9 @@ import "./Profile.css"
 import { useNavigate } from "react-router-dom"
 import { userData } from "../../app/slices/userSlice"
 import { useSelector } from "react-redux"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
+import { CInput } from "../../common/CInput/CInput";
+import { GetProfile } from "../../services/apiCalls"
 
 export const Profile = () => {
 
@@ -12,17 +14,75 @@ export const Profile = () => {
 
     const reduxUser = useSelector(userData)
 
+    const [loadedData, setLoadedData] = useState(false)
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+    })
+
+    const inputHandler = (e) => {
+        setUser((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
     useEffect(() => {
         if (!reduxUser.credentials.token) {
             navigate("/")
         }
-    },[reduxUser])
+    }, [reduxUser])
+
+    useEffect(() => {
+
+        const getUserProfile = async () => {
+            try {
+
+                const fetched = await GetProfile(reduxUser.credentials.token)
+                console.log(fetched, "goasdas");
+
+                setLoadedData(true)
+
+                setUser({
+                    name: fetched.data[0].name,
+                    email: fetched.data[0].email,
+                })
+
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        if (!loadedData) {
+            getUserProfile()
+        }
+
+    }, [user])
 
     return (
         <>
-        <div className="profileDesign">
-            Profile
-        </div>
+            <div className="profileDesign">
+                <CInput
+                    className={`cInputDesign`}
+                    type={"text"}
+                    placeholder={""}
+                    name={"name"}
+                    value={user.name || ""}
+                    disabled={"disabled"}
+                    onChangeFunction={(e) => inputHandler(e)}
+                // onBlurFunction={(e) => checkError(e)}
+                />
+                <CInput
+                    className={`cInputDesign`}
+                    type={"email"}
+                    placeholder={""}
+                    name={"email"}
+                    value={user.email || ""}
+                    disabled={"disabled"}
+                    onChangeFunction={(e) => inputHandler(e)}
+                // onBlurFunction={(e) => checkError(e)}
+                />
+            </div>
         </>
     )
 }
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -48,3 +48,28 @@ export const RegisterUser = async (user) => {
         return error;
     }
 }
+
+export const GetProfile = async (token) => {
+
+    const options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+      };
+    
+      try {
+        const response = await fetch(`${root}users/profile`, options);
+    
+        const data = await response.json();
+    
+        if (!data.success) {
+          throw new Error(data.message);
+        }
+    
+        return data;
+      } catch (error) {
+        return error;
+      }
+  }
