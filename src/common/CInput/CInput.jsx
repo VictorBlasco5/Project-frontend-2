d887094aac688455ae7cf5bffeb94e3aa15d68ef
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-06 11:19:40 +0200

    feat: registerUser function in apiCalls and logic implemented in register

--- src/common/CInput/CInput.jsx
+++ src/common/CInput/CInput.jsx
@@ -1,13 +1,16 @@
 import "./CInput.css"
 
-export const CInput = ({ type, name, value, changeEmit}) => {
+export const CInput = ({ placeholder, type, name, value, disabled, changeEmit, onBlurFunction}) => {
     return (
         <input 
         className="cInputDesign"
+        placeholder={placeholder}
         type={type}
         name={name}
         value={value}
+        disabled={disabled}
         onChange={(e)=>changeEmit(e)}
+        onBlur={onBlurFunction}
         />
     )
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,77 @@
 import "./Register.css"
+import { CInput } from "../../common/CInput/CInput"
+import { useState } from "react"
+import { RegisterUser } from "../../services/apiCalls";
+import { useNavigate } from "react-router-dom";
+
+
 export const Register = () => {
+
+    const navigate = useNavigate()
+
+    const [user, setUser] = useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const imputHandler = (e) => {
+        setUser(
+            (prevState) => ({
+                ...prevState,
+                [e.target.name]: e.target.value,
+            })
+        )
+    }
+
+    const registerMe = async () => {
+
+        try {
+
+            const fetched = await RegisterUser(user);
+            console.log(fetched);
+
+            setTimeout(() => {
+                navigate("/")
+            }, 1200)
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
 
     return (
 
         <>
             <div className="registerDesign">
-                register
+                <CInput
+                    placeholder={"Name"}
+                    type={"text"}
+                    name={"name"}
+                    value={user.name || ""}
+                    changeEmit={(e) => imputHandler(e)}
+
+                />
+
+                <CInput
+                    placeholder={"Email"}
+                    type={"email"}
+                    name={"email"}
+                    value={user.email || ""}
+                    changeEmit={(e) => imputHandler(e)}
+
+                />
+
+                <CInput
+                    placeholder={"Password"}
+                    type={"password"}
+                    name={"password"}
+                    value={user.password || ""}
+                    changeEmit={(e) => imputHandler(e)}
+
+                />
+
+                <button className="buttonLogin" onClick={registerMe}>Register</button>
             </div>
 
         </>
