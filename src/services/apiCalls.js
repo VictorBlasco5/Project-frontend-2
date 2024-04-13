7eb67083c7b1eb99a818fd033f37cf0a2a59d12d
VíctorBlasco5 <victor.blasco.17@gmail.com>
2024-04-13 12:56:54 +0200

    feat: add use-select none in all views

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -257,27 +257,27 @@ export const GetUsers = async (token) => {
     }
 }
 
-export const DeleteUsers = async (userId, token ) => {
+export const DeleteUsers = async (userId, token) => {
 
     const options = {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`
-      },
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
     };
-  
+
     try {
-      const response = await fetch(`${root}users/${userId}`, options);
-  
-      const data = await response.json();
-  
-      if (!data.success) {
-        throw new Error(data.message);
-      }
-  
-      return data;
+        const response = await fetch(`${root}users/${userId}`, options);
+
+        const data = await response.json();
+
+        if (!data.success) {
+            throw new Error(data.message);
+        }
+
+        return data;
     } catch (error) {
-      return error;
+        return error;
     }
-  }
+}
